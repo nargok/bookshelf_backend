0bac03a6b91c99836cd6175c9f225b1254be8aa4
nargok <nargok@gmail.com>
2023-10-22 19:43:36 +0900

    add jooq

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("nu.studer.jooq") version "8.0" // これを追加
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
 }
@@ -24,6 +25,11 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // jooq
+    jooqGenerator("com.mysql:mysql-connector-j")
+    jooqGenerator("jakarta.xml.bind:jakarta.xml.bind-api:4.0.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -36,4 +42,34 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+jooq {
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                jdbc.apply {
+                    url = "jdbc:mysql://localhost:3306/library?enabledTLSProtocols=TLSv1.2"
+                    user = "root"
+                    password = "root"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.KotlinGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.mysql.MySQLDatabase"
+                        inputSchema = "library"
+                        excludes = "flyway_schema_history"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isTables = true
+                    }
+                    target.apply {
+                        packageName = "com.example.ktknowledgeTodo.infra.jooq"
+                        directory = "$buildDir/generated/source/jooq/main"
+                    }
+                }
+            }
+        }
+    }
 }
